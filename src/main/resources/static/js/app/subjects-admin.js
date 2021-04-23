ba90b307c518490ccd6655006227b1b711e7602e
Err0rCode7 <aa6184708@naver.com>
2021-04-23 17:29:15 +0900

    # Add subejct page

--- src/main/resources/static/js/app/subjects-admin.js
+++ src/main/resources/static/js/app/subjects-admin.js
@@ -8,10 +8,6 @@ var main = {
         $('#btn-update').on('click', function () {
             _this.update();
         });
-
-        $('#btn-delete').on('click', function () {
-            _this.delete();
-        });
     },
     save : function () {
         var data = {
@@ -52,20 +48,6 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('서브젝트가 수정되었습니다.');
-            window.location.href = '/admin/subjects';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    },
-    delete : function() {
-        var id = $('#id').val();
-        $.ajax({
-            type: 'DELETE',
-            url: '/api/v1/subjects/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8'
-        }).done(function() {
-            alert('글이 삭제되었습니다.');
             window.location.href = '/admin/subjects';
         }).fail(function (error) {
             alert(JSON.stringify(error));
