b3b18ab49df29986e93973963b654f158668a510
Err0rCode7 <aa6184708@naver.com>
2021-04-23 17:30:43 +0900

    # Add page service, Add exception

--- src/main/java/com/codetogether/openstudio/service/SubjectService.java
+++ src/main/java/com/codetogether/openstudio/service/SubjectService.java
@@ -1,12 +1,17 @@
 package com.codetogether.openstudio.service;
 
+import com.codetogether.openstudio.domain.Member;
 import com.codetogether.openstudio.domain.Subject;
+import com.codetogether.openstudio.dto.member.MemberListResponseDto;
 import com.codetogether.openstudio.dto.subject.SubjectListResponseDto;
 import com.codetogether.openstudio.dto.subject.SubjectResponseDto;
 import com.codetogether.openstudio.dto.subject.SubjectSaveRequestDto;
 import com.codetogether.openstudio.dto.subject.SubjectUpdateRequestDto;
 import com.codetogether.openstudio.repository.SubjectRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,17 +38,35 @@ public class SubjectService {
     }
 
     @Transactional(readOnly = true)
+    public Page<SubjectListResponseDto> findAllDesc(Pageable pageable) {
+        Page<Subject> subjectPage = subjectRepository.findAllDesc(pageable);
+        int totalElements = (int) subjectPage.getTotalElements();
+        return new PageImpl<>(subjectPage
+                .getContent().stream()
+                .map(SubjectListResponseDto::new)
+                .collect(Collectors.toList()), pageable, totalElements);
+    }
+
+    @Transactional(readOnly = true)
     public List<Subject> findAllEntity() {
         return subjectRepository.findAll();
     }
 
     @Transactional
     public Long save(SubjectSaveRequestDto requestDto) {
+        subjectRepository.findByName(requestDto.getName())
+                .ifPresent((subject -> {
+                    throw new IllegalArgumentException("같은 이름의 서브젝트가 이미 존재합니다.");
+                }));
         return subjectRepository.save(requestDto.toEntity()).getId();
     }
 
     @Transactional
     public Long save(Subject subject) {
+        subjectRepository.findByName(subject.getName())
+                .ifPresent((entity -> {
+                    throw new IllegalArgumentException("같은 이름의 서브젝트가 이미 존재합니다.");
+                }));
         return subjectRepository.save(subject).getId();
     }
 
@@ -55,6 +78,7 @@ public class SubjectService {
         return id;
     }
 
+    @Transactional
     public void delete(Long id) {
         Subject subject = subjectRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 서브젝트가 없습니다. id = " + id));
