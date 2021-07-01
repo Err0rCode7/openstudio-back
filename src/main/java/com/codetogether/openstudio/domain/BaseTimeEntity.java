3dcbb82bf09de3f5f7e50d17a8e16fbb51c99ae6
Err0rCode7 <aa6184708@naver.com>
2021-07-01 16:11:21 +0900

    # Update datetime to timestamp

--- src/main/java/com/codetogether/openstudio/domain/BaseTimeEntity.java
+++ src/main/java/com/codetogether/openstudio/domain/BaseTimeEntity.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -16,8 +17,10 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(columnDefinition = "TIMESTAMP", nullable = false)
     private LocalDateTime modifiedAt;
 }
